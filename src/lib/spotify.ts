c90be8f1a65b68a44ed93d9a5800a069f5d68858
meesterstijn <meesterstijnvdwulp@gmail.com>
2026-05-21 16:38:44 +0200

    Update spotify.ts

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,5 +1,5 @@
 const CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID as string;
-const REDIRECT_URI = window.location.origin + "/";
+const REDIRECT_URI = import.meta.env.VITE_SPOTIFY_REDIRECT_URI as string;
 const SCOPES = [
   "user-read-currently-playing",
   "user-read-playback-state",
