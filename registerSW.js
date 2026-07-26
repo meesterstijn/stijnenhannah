09e436116d9f55ecc05c3b6cbb36bcb49aa84f31
meesterstijn <meesterstijnvdwulp@gmail.com>
2026-07-26 10:09:10 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/stijnenhannah/sw.js', { scope: '/stijnenhannah/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
