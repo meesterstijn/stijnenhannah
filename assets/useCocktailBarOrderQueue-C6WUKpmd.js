1add844c14079d154d8f94acf84cbc60b92e2216
meesterstijn <meesterstijnvdwulp@gmail.com>
2026-08-04 14:35:36 +0200

    Updates

--- assets/useCocktailBarOrderQueue-C6WUKpmd.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./query-Cycj_yO6.js";import{f as r}from"./orders-1cZKNvTh.js";const u=["cocktail_bar","orders","queue"];function a(){return e({queryKey:u,queryFn:r})}export{u as C,a as u};
