00d713a0e9a4b5cbf6f4569dd5184e2ef96158a7
meesterstijn <meesterstijn@users.noreply.github.com>
2026-08-10 14:01:19 +0000

    deploy: f28fcf8e8daa59776ea7a590044df4e451f85e06

--- assets/useCocktailBarOrderQueue-D5GSXH-4.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./query-Cycj_yO6.js";import{f as r}from"./orders-BoHeBse_.js";const u=["cocktail_bar","orders","queue"];function a(){return e({queryKey:u,queryFn:r})}export{u as C,a as u};
