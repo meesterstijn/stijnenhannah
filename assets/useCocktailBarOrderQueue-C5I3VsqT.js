76336e5d0b22560e93c70506d563b1b4399173b5
meesterstijn <150523262+meesterstijn@users.noreply.github.com>
2026-08-13 21:24:49 +0200

    Updates

--- assets/useCocktailBarOrderQueue-C5I3VsqT.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./query-BGuq1gWo.js";import{f as r}from"./orders-B66J9p9h.js";const u=["cocktail_bar","orders","queue"];function a(){return e({queryKey:u,queryFn:r})}export{u as C,a as u};
