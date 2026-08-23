d7772be5a1de142046dea8830c5123494895c854
meesterstijn <150523262+meesterstijn@users.noreply.github.com>
2026-08-23 21:58:03 +0200

    Updates

--- assets/GnV2Loading-BWiQvLQE.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./query-DRm75QKu.js";import{G as s}from"./GnV2Scene-DTcto1lH.js";function i(){return n.jsx(s,{className:"gnv2-loading-scene",children:n.jsx("div",{className:"gnv2-loading",children:n.jsx("div",{className:"gnv2-loading-pulse","aria-hidden":!0})})})}export{i as G};
--- assets/gameCoverStorage-D_VXeKIU.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index-5vg99YBo.js";const t="game-night-covers";function o(r){return e.storage.from(t).getPublicUrl(r).data.publicUrl}export{o as g};
