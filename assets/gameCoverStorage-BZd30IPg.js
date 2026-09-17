bcda0180ff432ead3bf8ef17fc3e3a89572a1932
meesterstijn <150523262+meesterstijn@users.noreply.github.com>
2026-09-17 15:12:21 +0200

    Updates

--- assets/gameCoverStorage-BZd30IPg.js
+++
@@ -1,1 +0,0 @@
-import{s as e}from"./index-DH24BESb.js";const t="game-night-covers";function o(r){return e.storage.from(t).getPublicUrl(r).data.publicUrl}export{o as g};
--- assets/useCocktailBarOrderQueue-DGcHN_Xb.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./query-DRm75QKu.js";import{f as r}from"./orders-DfO9dJsP.js";const u=["cocktail_bar","orders","queue"];function a(){return e({queryKey:u,queryFn:r})}export{u as C,a as u};
